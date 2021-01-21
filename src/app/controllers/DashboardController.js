1a8764f4daee3fd36217eb040348950bed6a5ff5
Antonio Sousa <antonio.falves@totvs.com.br>
2021-01-21 20:10:05 -0300

    dashboard api returning the badges

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,10 +1,30 @@
 const Collaborator = require('../models/CollaboratorModel');
+const Achieviment = require('../models/AchiviementModel');
+const CategoriesType = require('../../contants/CategoriesType');
 
 class DashboardController {
     async index(request, response) {
         const userIdLogged = request.userIdLogged;
-        const { own_claps, available_claps } = await Collaborator.findOne( {user: userIdLogged });
-        return response.json({ own_claps, available_claps });
+
+        const { own_claps, available_claps } = await Collaborator.findOne({ user: userIdLogged });
+
+        const badges = Object.values(CategoriesType).map(category => ({
+            _id: String(Date.now()),
+            category_type: category,
+            level: 0,
+        }));
+
+        const achieviments = await Achieviment.find({ collaborator: userIdLogged });
+
+        achieviments.forEach(achievement => {
+            badges.forEach((badge, badgeIndex) => {
+                if (achievement.name === badge.category_type) {
+                    badges[badgeIndex] = achievement;
+                }
+            });
+        });
+
+        return response.json({ own_claps, available_claps, badges });
     }
 }
 
