cde77d5ead81b987c8d407e5e0b66ffda59359ea
Adolfo Quaranta <adolfofq@gmail.com>
2021-01-21 16:22:05 -0300

    add CollaboratorModel

---
+++ src/app/validators/Collaborator.js
@@ -0,0 +1,9 @@
+const {Joi} = require('express-validation');
+
+const Collaborator = {
+    body: Joi.object({
+        name: Joi.string().min(3).required(),
+    }),
+}
+
+module.exports = Collaborator;
