c5b2e342430a24d9bd395f5347148ba4a82118bf
Antonio Sousa <antonio.falves@totvs.com.br>
2021-01-21 14:25:44 -0300

    avoid users duplicated

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,7 +2,13 @@ const User = require('../models/UserModel');
 
 class UserController {
     async store(request, response) {
-        const { _id, username } = await User.create(request.body);
+        const { username } = request.body;
+
+        if (await User.findOne({ username })) {
+            return response.status(401).json({ error: 'User already exists' });
+        }
+
+        const { _id } = await User.create(request.body);
         return response.json({ _id, username });
     }
 }
