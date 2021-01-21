76b9b2cb9ea656a349ef62d7e310636d9fb37368
Antonio Sousa <antonio.falves@totvs.com.br>
2021-01-21 11:58:20 -0300

    created mongodb connection

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
 require('dotenv/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
+const mongodbConfig = require('./config/mongodb');
 
 class App {
     constructor() {
         this.server = express();
 
         this.middlewares();
+        this.database();
         this.routes();
     }
 
     middlewares() {
         this.server.use(express.json());
+    }
+
+    database() {
+        mongoose.connect(mongodbConfig.uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
     }
 
     routes() {
