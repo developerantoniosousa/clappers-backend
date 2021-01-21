6f15422c092c06b08214b4ca503d55d7e56276cb
Antonio Sousa <antonio.falves@totvs.com.br>
2021-01-21 15:46:44 -0300

    normalize username using lowercase and trim

--- src/app/models/UserModel.js
+++ src/app/models/UserModel.js
@@ -8,7 +8,9 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
         type: String,
