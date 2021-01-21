92c08ec5d2dac0c6403aff313e43089e3f583319
Antonio Sousa <antonio.falves@totvs.com.br>
2021-01-21 15:11:05 -0300

    authentication middleware created

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,22 @@
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../../config/auth');
+
+module.exports = async (request, response, next) => {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        return response.status(401).json({ error: 'Access token not found' });
+    }
+
+    const [,token] = authHeader.split(' ');
+
+    try {
+        const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+        request.userIdLogged = decoded._id;
+        return next();
+    } catch {
+        return response.status(401).json({ error: 'Access token is invalid' });
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const asyncHandler = require('express-async-handler');
 
 const controllers = require('./app/controllers');
 const validators = require('./app/validators');
+const middlewares = require('./app/middlewares');
 
 const { Router } = express;
 
@@ -12,5 +13,8 @@ const router = new Router();
 router.post('/users', validate(validators.User), asyncHandler(controllers.UserController.store));
 
 router.post('/sessions', validate(validators.Session), asyncHandler(controllers.SessionController.store));
+
+// all routes below must have to receive the access token
+router.use(middlewares.auth);
 
 module.exports = router;
