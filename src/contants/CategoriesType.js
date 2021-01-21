7cc03d588afe92f3c8a1ae534612923f039d72a3
Antonio Sousa <antonio.falves@totvs.com.br>
2021-01-21 20:09:09 -0300

    added categories type constant

---
+++ src/contants/CategoriesType.js
@@ -0,0 +1,11 @@
+module.exports = {
+    DISSEMINATE_KNOWLEDGE: 'disseminate-knowledge',
+    THINK_OUT_THE_BOX: 'think-out-the-box',
+    COMMUNICATION: 'communication',
+    TEAM_WORK: 'team-work',
+    RESILIENCE: 'resilience',
+    PROACTIVITY: 'proactivity',
+    ASSERTIVENESS: 'assertiveness',
+    PRODUCTIVITY: 'productivity',
+    COMMITMENT: 'commitment',
+}
