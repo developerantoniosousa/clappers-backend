fa2ba7008fa573f75305a45f5c8b6f8034aa4947
Antonio Sousa <antonio.falves@totvs.com.br>
2021-01-22 11:50:48 -0300

    list timeline events by collaboratorId instead of userId

--- src/app/controllers/TimelineController.js
+++ src/app/controllers/TimelineController.js
@@ -1,9 +1,13 @@
+const Collaborator = require('../models/CollaboratorModel');
 const Event = require('../models/EventModel');
 
 class TimelineController {
     async index(request, response) {
         const userIdLogged = request.userIdLogged;
-        const events = await Event.find({ collaborator: userIdLogged });
+
+        const collaborator = await Collaborator.findOne({ user: userIdLogged });
+
+        const events = await Event.find({ collaborator: collaborator._id });
         return response.json(events);
     }
 }
