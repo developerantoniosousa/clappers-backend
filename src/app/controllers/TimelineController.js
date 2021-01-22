abe6d9960ef57c9e7fccb75c753d2f6d15f3de60
Adolfo Quaranta <adolfofq@gmail.com>
2021-01-22 11:17:08 -0300

    add sort to collaborator and timeline queries

--- src/app/controllers/TimelineController.js
+++ src/app/controllers/TimelineController.js
@@ -3,7 +3,7 @@ const Event = require('../models/EventModel');
 class TimelineController {
     async index(request, response) {
         const userIdLogged = request.userIdLogged;
-        const events = await Event.find({ collaborator: userIdLogged });
+        const events = await Event.find({ collaborator: userIdLogged }).sort({createdAt: 1});
         return response.json(events);
     }
 }
