4ebab24da60746aad3e472083e91054e9f0d287b
Antonio Sousa <antonio.falves@totvs.com.br>
2021-01-22 11:37:28 -0300

    record an event for timeline when new claps were deliveried

--- src/app/controllers/DeliveryClapController.js
+++ src/app/controllers/DeliveryClapController.js
@@ -1,13 +1,29 @@
 const Collaborator = require('../models/CollaboratorModel');
+const Event = require('../models/EventModel');
+
+const EventsType = require('../../contants/EventsType');
 
 class DeliveryClapController {
     async update(request, response) {
         const { leader, claps } = request.body;
-        const updateConditions = { leader };
+        const conditions = { leader };
         const updateData = { available_claps: claps };
         const updateOptions = { multi: true, upsert: true };
 
-        await Collaborator.updateMany(updateConditions, updateData, updateOptions);
+        await Collaborator.updateMany(conditions, updateData, updateOptions);
+
+        const collaborators = await Collaborator.find(conditions);
+
+        const events = collaborators.map(collaborator => {
+            return {
+                content: {
+                    claps
+                },
+                type: EventsType.START_TIME_CYCLE,
+                collaborator: collaborator._id,
+            }
+        });
+        await Event.insertMany(events);
 
         return response.json({ message: 'Claps were delivered with success' });
     }
