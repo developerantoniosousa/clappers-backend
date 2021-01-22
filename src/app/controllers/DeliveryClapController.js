b609433593cdf2d726b8c288b575f9c4d5b822ca
Antonio Sousa <antonio.falves@totvs.com.br>
2021-01-22 13:16:42 -0300

    allow only leaders user to delivery claps

--- src/app/controllers/DeliveryClapController.js
+++ src/app/controllers/DeliveryClapController.js
@@ -6,9 +6,14 @@ const EventsType = require('../../contants/EventsType');
 class DeliveryClapController {
     async update(request, response) {
         const { leader, claps } = request.body;
+        const userRoleLogged = request.userRoleLogged;
         const conditions = { leader };
         const updateData = { available_claps: claps };
         const updateOptions = { multi: true, upsert: true };
+
+        if (userRoleLogged !== 'leader') {
+            return response.status(403).json({ error: 'You do not have authorization to delivery claps' });
+        }
 
         await Collaborator.updateMany(conditions, updateData, updateOptions);
 
