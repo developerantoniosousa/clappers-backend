44f0dd9d2e8919e1d3702c801e389333b060136a
Antonio Sousa <developerantoniosousa@gmail.com>
2021-06-18 23:03:35 -0300

    fix: request.userRoleLogged not defined

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -10,11 +10,12 @@ module.exports = async (request, response, next) => {
         return response.status(401).json({ error: 'Access token not found' });
     }
 
-    const [,token] = authHeader.split(' ');
+    const [, token] = authHeader.split(' ');
 
     try {
         const decoded = await promisify(jwt.verify)(token, authConfig.secret);
         request.userIdLogged = decoded._id;
+        request.userRoleLogged = decoded.role;
         return next();
     } catch {
         return response.status(401).json({ error: 'Access token is invalid' });
